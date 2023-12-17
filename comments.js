0ac489fe3c22a8978507e487c74226f3fafb2bde
chellyang <83898780+chellyang@users.noreply.github.com>
2023-12-17 03:36:09 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,64 @@
 // 创建Web服务器
 const express = require('express');
 const router = express.Router();
-const commentsCtrl = require('../controller/commentsCtrl');
+const pool = require('../pool');
 
-// 评论列表
-router.get('/comments',commentsCtrl.commentsList);
-// 添加评论
-router.post('/comments',commentsCtrl.commentsAdd);
-// 删除评论
-router.delete('/comments/:id',commentsCtrl.commentsDel);
-// 评论状态
-router.put('/comments/:id',commentsCtrl.commentsStatus);
-// 评论详情
-router.get('/comments/:id',commentsCtrl.commentsDetail);
-// 评论修改
-router.put('/comments',commentsCtrl.commentsEdit);
+// 1.添加评论
+router.post('/add', (req, res) => {
+    let obj = req.body;
+    console.log(obj);
+    if (!obj.uname) {
+        res.send({ code: 401, msg: 'uname required' });
+        return;
+    }
+    if (!obj.content) {
+        res.send({ code: 402, msg: 'content required' });
+        return;
+    }
+    if (!obj.cid) {
+        res.send({ code: 403, msg: 'cid required' });
+        return;
+    }
+    let sql = 'INSERT INTO comments SET ?';
+    pool.query(sql, obj, (err, result) => {
+        if (err) throw err;
+        if (result.affectedRows > 0) {
+            res.send({ code: 200, msg: 'add success' });
+        } else {
+            res.send({ code: 301, msg: 'add error' });
+        }
+    });
+});
 
-module.exports = router;
+// 2.查询评论
+router.get('/list', (req, res) => {
+    let cid = req.query.cid;
+    let sql = 'SELECT * FROM comments WHERE cid=? ORDER BY ctime DESC';
+    pool.query(sql, [cid], (err, result) => {
+        if (err) throw err;
+        if (result.length > 0) {
+            res.send({ code: 200, msg: 'list success', data: result });
+        } else {
+            res.send({ code: 301, msg: 'list error' });
+        }
+    });
+});
+
+// 3.删除评论
+router.get('/delete', (req, res) => {
+    let cid = req.query.cid;
+    let sql = 'DELETE FROM comments WHERE cid=?';
+    pool.query(sql, [cid], (err, result) => {
+        if (err) throw err;
+        if (result.affectedRows > 0) {
+            res.send({ code: 200, msg: 'delete success' });
+        } else {
+            res.send({ code: 301, msg: 'delete error' });
+        }
+    });
+});
+
+// 4.查询所有评论
+router.get('/all', (req, res) => {
+    let sql = 'SELECT * FROM comments ORDER BY ctime DESC';
+    pool.query(sql,
