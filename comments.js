be71d37b9910acc7532b77a43bfeff4e98cf31b9
chellyang <83898780+chellyang@users.noreply.github.com>
2023-12-17 03:28:55 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,19 @@
 // 创建Web服务器
 const express = require('express');
 const router = express.Router();
-const comment = require('../controllers/comment');
-// 挂载路由
-// 发布评论
-router.post('/post', comment.postComment);
-// 获取评论
-router.get('/list', comment.getCommentList);
-// 导出路由
+const commentsCtrl = require('../controller/commentsCtrl');
+
+// 评论列表
+router.get('/comments',commentsCtrl.commentsList);
+// 添加评论
+router.post('/comments',commentsCtrl.commentsAdd);
+// 删除评论
+router.delete('/comments/:id',commentsCtrl.commentsDel);
+// 评论状态
+router.put('/comments/:id',commentsCtrl.commentsStatus);
+// 评论详情
+router.get('/comments/:id',commentsCtrl.commentsDetail);
+// 评论修改
+router.put('/comments',commentsCtrl.commentsEdit);
+
 module.exports = router;
